10849483d2d7fc2dc4c0b41f8d41bb348ade0f0c
Elise <pixbl3@ymail.com>
2022-03-12 12:57:15 -0700

    Added updates... New colors, new html files, JS

--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@ import _ from 'lodash';
 
 import ".//style.scss";
 
+var modal = document.getElementById('id01');
+
+
 function component() {
     const element = document.createElement('div');
   
  // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join([], ' ');
   
     return element;
   }
+
+  var post= document.getElementById("post");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    window.alert("Sorry... Comments are not enabled at this time.");
+ 
+});
   
   document.body.appendChild(component());
+  
